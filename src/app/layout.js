6ee3524cd214c4c8894e3bd25e3486d954997f9e
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-02-27 16:54:41 +0530

    paste adsense code

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<head>
+				<script
+					async
+					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6367781538880500'
+					crossorigin='anonymous'></script>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<AuthProvider>
